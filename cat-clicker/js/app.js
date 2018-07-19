d86d623b70f43c67117f77c2cbb9a33f29186388
Sean Morrison <sean1972@gmail.com>
2018-07-18 21:47:36 -0700

    Updated the playable cat clicker with 2 cats

--- cat-clicker/js/app.js
+++ cat-clicker/js/app.js
@@ -1,7 +1,22 @@
 /*
+ * Add cat headings above images.
+ */
+$('#milo-heading').html('Milo the Cat');
+$('#macy-heading').html('Macy the Cat');
+
+/*
  * Select and bind click event handler to milo class.
  */
-$("#milo").click(function(){
+$("#milo-image").click(function(){
+  // parse string in count class
+  let count = parseInt($(".count").text());
+  // increment count
+  count++;
+  // display count
+  $(".count").text(count);
+});
+
+$("#macy-image").click(function(){
   // parse string in count class
   let count = parseInt($(".count").text());
   // increment count
